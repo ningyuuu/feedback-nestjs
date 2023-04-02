c4dc22662759c1e5b80906c8b41e98fcb95f9bc6
ningyu <ningyu@estl.sg>
2023-04-02 19:47:11 +0800

    remove gradings apis

--- src/gradings/gradings.controller.ts
+++ src/gradings/gradings.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { GradingsService } from './gradings.service';
 import { CreateGradingDto } from './dto/create-grading.dto';
-import { UpdateGradingDto } from './dto/update-grading.dto';
 
 @Controller('gradings')
 export class GradingsController {
@@ -20,15 +19,5 @@ export class GradingsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.gradingsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGradingDto: UpdateGradingDto) {
-    return this.gradingsService.update(+id, updateGradingDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gradingsService.remove(+id);
   }
 }
