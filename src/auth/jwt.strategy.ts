a9de8247833e897697af2c272fee429af182c6d1
ningyu <ningyu@estl.sg>
2023-04-07 16:54:47 +0800

    remove console log from auth

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log({ auth_payload: payload });
     return { userId: payload.sub, login: payload.login };
   }
 }
