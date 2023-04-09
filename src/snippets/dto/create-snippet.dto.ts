e6486f73ab42cf6ad996db92547e07614142a28c
ningyuuu <ningyu321@gmail.com>
2023-04-09 22:32:17 +0800

    make project id optional in create-snippet.dto

--- src/snippets/dto/create-snippet.dto.ts
+++ src/snippets/dto/create-snippet.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateSnippetDto {
   @ApiProperty()
-  project: number;
+  project?: number;
 
   @ApiPropertyOptional()
   assignment?: number;
